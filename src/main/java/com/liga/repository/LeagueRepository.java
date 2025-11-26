82e11772b89c447f9bb03c876af2847f8433cd2b
roberto-palmi <robpalmar@alu.edu.gva.es>
2025-11-26 19:49:26 +0100

    Creado League Repository

--- src/main/java/com/liga/repository/LeagueRepository.java
+++ src/main/java/com/liga/repository/LeagueRepository.java
@@ -1,4 +1,19 @@
 package com.liga.repository;
 
-public class LeagueRepository {
+import com.liga.model.Equipo;
+import com.liga.model.Jugador;
+import com.liga.model.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LeagueRepository {
+        List<Equipo> listarEquipos();
+        Optional<Equipo> buscarEquipoPorId(String id);
+        List<Jugador> listarJugadores();
+        List<Jugador> buscarJugadorPorEquipo(String equipoId);
+        Optional<Jugador> buscarJugadorPorId(String id);
+        List<Usuario> listarUsuarios();
+        Optional<Usuario> buscarUsuarioPorId(String id);
+        void guardarUsuarios(List<Usuario> usuarios);
 }
--- src/main/java/com/liga/repository/LeagueRepositoryImpl.java
+++ src/main/java/com/liga/repository/LeagueRepositoryImpl.java
@@ -1,4 +1,51 @@
 package com.liga.repository;
 
-public class LeagueRepositoryImpl {
+import com.liga.model.Equipo;
+import com.liga.model.Jugador;
+import com.liga.model.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LeagueRepositoryImpl implements LeagueRepository {
+
+    @Override
+    public List<Equipo> listarEquipos() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Equipo> buscarEquipoPorId(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Jugador> listarJugadores() {
+        return List.of();
+    }
+
+    @Override
+    public List<Jugador> buscarJugadorPorEquipo(String equipoId) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Jugador> buscarJugadorPorId(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Usuario> listarUsuarios() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Usuario> buscarUsuarioPorId(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void guardarUsuarios(List<Usuario> usuarios) {
+
+    }
 }
