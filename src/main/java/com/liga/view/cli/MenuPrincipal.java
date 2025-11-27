c59de61d9905d219af8e62eca200412cf163e2d6
ezequielmenor <ezequielmenor@gmail.com>
2025-11-27 09:13:46 +0100

    Implementación Buscar jugadores por nombre y posición E7

--- src/main/java/com/liga/view/cli/MenuPrincipal.java
+++ src/main/java/com/liga/view/cli/MenuPrincipal.java
@@ -5,10 +5,11 @@ public class MenuPrincipal {
     public void mostrarMenu() {
         System.out.println("=== LIGA FANTASY ===");
         System.out.println("1. Usuarios");
-        System.out.println("2. Equipos/Jugadores");
-        System.out.println("3. Mercado");
-        System.out.println("4. Alineaciones");
-        System.out.println("5. Jornadas");
+        System.out.println("2. Equipos");
+        System.out.println("3. Jugadores");
+        System.out.println("4. Mercado");
+        System.out.println("5. Alineaciones");
+        System.out.println("6. Jornadas");
         System.out.println("0. Salir");
         System.out.print("Elige una opción: ");
     }
